cbe10b4d7ab4ba5e532c53cc7ac46c2405f03907
Alisina <sayedalisinaa@gmail.com>
2023-05-22 19:37:31 +0200

    initial commit from local

---
+++ SixHoursStudy/SixHoursStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SixHoursStudyApp.swift
+//  SixHoursStudy
+//
+//  Created by Alisina on 22.05.23.
+//
+
+import SwiftUI
+
+@main
+struct SixHoursStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
